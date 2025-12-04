973f36eed810d1b86aa0c9633622a10a3fc1309c
TuansTran <148312811+TuansTran@users.noreply.github.com>
2025-12-04 15:47:42 +0700

    First commit to branch be-feat

--- fitness_ai_assistant/n8n_model_assistant/temp_python.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="intfloat/multilingual-e5-large",
-    local_dir="./models/e5-large",
-    local_dir_use_symlinks=False
-)
